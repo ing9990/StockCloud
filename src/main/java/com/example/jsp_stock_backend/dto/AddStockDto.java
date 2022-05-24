5acc07ce7a81ba443071d357f9951d097efd8d1f
TaeWoo <gimgau0218@naver.com>
2022-05-24 19:46:01 +0900

    파이썬 requests로 보낸 주식 정보들을 매핑.

--- src/main/java/com/example/jsp_stock_backend/dto/AddStockDto.java
+++ src/main/java/com/example/jsp_stock_backend/dto/AddStockDto.java
@@ -1,9 +1,9 @@
 package com.example.jsp_stock_backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
 
 /**
  * @author TaeWK
@@ -12,12 +12,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
+@Slf4j
 @AllArgsConstructor
 public class AddStockDto {
-    private int stock_rank;       //주식 순위
-    private String stock_name;    //주식 이름
-    private int stock_price;      //가격
-    private int change_price_day; //전일비
-    private double chage_per_day; //등락률
-    private int trading_volume;   //거래량
+
+    private ArrayList<String> stock_name;         //주식 이름
+
+    private ArrayList<Integer> stock_rank;        //주식 순위
+
+    private ArrayList<Integer> stock_price;       //가격
+
+    private ArrayList<Integer> change_price_day;  //전일비
+
+    private ArrayList<Double> chage_per_day;      //등락률
+
+    private ArrayList<Integer> total_price;       //시가총액
+
+    private ArrayList<Integer> trading_volume;    //거래량
 }

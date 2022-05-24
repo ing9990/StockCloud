bce4fe81c3cf351f1d6cb9682bf57b77eeea9a56
TaeWoo <gimgau0218@naver.com>
2022-05-24 19:46:20 +0900

    Controller 추가.

--- src/main/java/com/example/jsp_stock_backend/controller/MainController.java
+++ src/main/java/com/example/jsp_stock_backend/controller/MainController.java
@@ -22,58 +22,62 @@ public class MainController {
     private final UserService userService;
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
 
 
     // -----------ADMIN GET METHOD-----------------//
-    
+
     @GetMapping("/users") // 유저 리스트 조회
-    public List<User> findAllUsers(){
+    public List<User> findAllUsers() {
         return userService.findAllUsers();
     }
 
     @GetMapping("/user/id/{id}") // 유저 아이디로 검색
-    public User findUserById(@PathVariable Long id){
+    public User findUserById(@PathVariable Long id) {
         return userService.findUserById(id);
     }
 
     @GetMapping("/user/name/{username}") // 유저 이름으로 검색
-    public List<User> findUsersByUsername(@PathVariable String username){
+    public List<User> findUsersByUsername(@PathVariable String username) {
         return userService.findUsersByUsername(username);
     }
 
 
     // -----------USERS GET METHOD-----------------//
     @GetMapping("") // 워드 클라우드 생성.
-    public void makeWordCloud(){
+    public void makeWordCloud() {
+        log.info("워드 클라우드 생성 중..");
         userService.makeWordCloud();
     }
 
+
     // --------------POST METHOD-------------- //
     @PostMapping("") // 유저 회원가입.
-    public void addUser(@RequestBody AddUserDto addUserDto){
+    public void addUser(@RequestBody AddUserDto addUserDto) {
         userService.addUser(addUserDto);
     }
 
+
     @PostMapping("/stock")
-    public List<AddStockDto> addStock(@RequestBody ArrayList<AddStockDto> addStockDto){
-        return addStockDto;
+    public String addStock(@RequestBody AddStockDto addStockDto) {
+        log.info("주식 가격들이 데이터베이스에 저장되었습니다.");
+        userService.stockParse(addStockDto);
+        return "200 OK";
     }
 
 
     // --------------PUT METHOD-------------- //
     @PutMapping("/{id}") // 유저 등급 상승.
-    public User editRoleById(@PathVariable Long id){
+    public User editRoleById(@PathVariable Long id) {
         return userService.editRole(id);
     }
 
 
-
     // -------------DELETE METHOD-------------- //
     @DeleteMapping("/{id}") // 유저 퇴출.
-    public void deleteUser(@PathVariable Long id){
+    public void deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
     }
 
